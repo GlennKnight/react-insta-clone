fa983af6488e1b75a2135b147a3a948a9f994efa
GlennKnight <48197163+GlennKnight@users.noreply.github.com>
2023-12-05 11:00:57 +0900

    Create AuthPage

---
+++ src/components/AuthForm/AuthForm.jsx
@@ -0,0 +1,42 @@
+import { Box, VStack, Image, Input, Button, Flex, Text } from '@chakra-ui/react';
+import { useState } from 'react';
+
+const AuthForm = () => {
+    const [isLogin, setIsLogin] = useState(true);
+    return (
+        <>
+            <Box border={'1px solid gray'} borderRadius={4} padding={5}>
+                <VStack spacing={4}>
+                    <Image src={'/logo.svg'} h={24} padding={2} cursor={'pointer'} alt='Logo' />
+                    <Input placeholder='Email' type='email' fontSize={14}></Input>
+                    <Input placeholder='Password' type='password' fontSize={14}></Input>
+                    {isLogin ? null : <Input placeholder='Confirm Password' type='password' fontSize={14}></Input>}
+                    <Button w={'full'} colorScheme='blue' size={'sm'} fontSize={14}>{isLogin ? 'Log in' : 'Sign up'}</Button>
+                    <Flex alignItems={'center'} justifyContent={'center'} my={4} gap={1} w={'full'}>
+                        <Box flex={2} h={'1px'} bg={'gray.400'}></Box>
+                        <Text mx={1} color={'white'}>Or</Text>
+                        <Box flex={2} h={'1px'} bg={'gray.400'}></Box>
+                    </Flex>
+                    <Flex alignItems={'center'} justifyContent={'center'} cursor={'pointer'}>
+                        <Image src='/google.svg' w={4} alt='Google logo' />
+                        <Text mx={2} color={'blue.500'}>Log in with Google</Text>
+                    </Flex>
+                </VStack>
+            </Box>
+
+            <Box border={'1px solid gray'} borderRadius={4} padding={5}>
+                <Flex alignItems={'center'} justifyContent={'center'}>
+                    <Box mx={2} fontSize={14}>
+                        {isLogin ? 'Don\'t have an account?' : 'Already have an account?'}
+                    </Box>
+                    <Box onClick={() => setIsLogin(!isLogin)} color={'blue.500'} cursor={'pointer'}>
+                        {isLogin ? 'Sign up' : 'Log in'}
+
+                    </Box>
+                </Flex>
+            </Box>
+        </>
+    )
+}
+
+export default AuthForm;
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,6 +1,25 @@
+import { Flex, Container, Image, Box, VStack } from '@chakra-ui/react';
+import AuthForm from '../../components/AuthForm/AuthForm';
+
 const AuthPage = () => {
     return (
-        <div>AuthPage</div>
+        <Flex minH={'100vh'} justifyContent={'center'} alignItems={'center'} px={4}>
+            <Container maxW={'container.md'} padding={0}>
+                <Flex justifyContent={'center'} alignItems={'center'} gap={10}>
+                    {/* Left hand-side */}
+                    <Box display={{ base: 'none', md: 'block' }}>
+                        <Image src='/blob.svg' height={650} alt='Blob' />
+                    </Box>
+                    {/* Right hand-side */}
+                    <VStack spacing={4} align={'stretch'}>
+                        <AuthForm />
+                        <Flex gap={5} justifyContent={'center'}>
+                            <Image src='/award.svg' h={16} alt='Award' />
+                        </Flex>
+                    </VStack>
+                </Flex>
+            </Container>
+        </Flex>
     )
 }
 
