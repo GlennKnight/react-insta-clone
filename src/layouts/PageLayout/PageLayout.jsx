18a8119bfd55957fae67649b4a0c7a7a5ce41353
GlennKnight <48197163+GlennKnight@users.noreply.github.com>
2023-12-06 15:38:55 +0900

    Added Navbar

--- src/layouts/PageLayout/PageLayout.jsx
+++ src/layouts/PageLayout/PageLayout.jsx
@@ -1,16 +1,21 @@
-import { Flex, Box } from '@chakra-ui/react'
+import { Flex, Box, Spinner } from '@chakra-ui/react'
 import Sidebar from '../../components/Sidebar/Sidebar';
 import { useLocation } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../firebase/firebase';
+import Navbar from '../../components/Navbar/Navbar';
 
 const PageLayout = ({ children }) => {
     const { pathname } = useLocation();
-    const [user] = useAuthState(auth);
+    const [user, loading] = useAuthState(auth);
     const canRenderSidebar = pathname !== '/auth' && user;
+    const canRenderNavbar = !user && !loading && pathname !== '/auth';
+
+    const checkingUserIsAuth = !user && loading;
+    if (checkingUserIsAuth) return <PageLayoutSpinner />;
 
     return (
-        <Flex>
+        <Flex flexDir={canRenderNavbar ? 'column' : 'row'}>
             {/* Sidebar on the left */}
             {canRenderSidebar ?
                 <Box w={{ base: '64px', md: '232px' }}>
@@ -18,8 +23,9 @@ const PageLayout = ({ children }) => {
                 </Box>
                 : null
             }
+            {/* Navbar */}
+            {canRenderNavbar ? <Navbar /> : null}
             {/* Page content on the right */}
-            {/* <Box flex={1} w={{ base: 'calc(100% - 70px)', md: 'calc(100% - 240px)' }}> */}
             <Box flex={1}>
                 {children}
             </Box>
@@ -28,3 +34,11 @@ const PageLayout = ({ children }) => {
 }
 
 export default PageLayout;
+
+const PageLayoutSpinner = () => {
+    return (
+        <Flex flexDir='column' h='100vh' alignItems='center' justifyContent='center'>
+            <Spinner size='xl' />
+        </Flex>
+    );
+};
