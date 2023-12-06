0f00f0abd325d9ff1f3e2f3663601332c8cf5198
GlennKnight <48197163+GlennKnight@users.noreply.github.com>
2023-12-06 18:42:37 +0900

    Added edit profile logic

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,13 +1,14 @@
-import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from '@chakra-ui/react';
+import { Avatar, AvatarGroup, Button, Flex, Text, VStack, useDisclosure } from '@chakra-ui/react';
 import useUserProfileStore from '../../store/userProfileStore';
 import useAuthStore from '../../store/authStore';
+import EditProfile from './EditProfile';
 
 const ProfileHeader = () => {
     const { userProfile } = useUserProfileStore();
     const authUser = useAuthStore((state) => state.user);
-    console.log('authUser', authUser)
     const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username;
     const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username;
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     return (
         <Flex gap={{ base: 4, sm: 10 }} py={10} direction={{ base: 'column', sm: 'row' }}>
@@ -18,7 +19,7 @@ const ProfileHeader = () => {
                 <Flex gap={4} direction={{ base: 'column', sm: 'row' }} justifyContent={{ base: 'center', sm: 'flex-start' }} w={'full'}>
                     <Text fontSize={{ base: 'sm', md: 'lg' }}>{userProfile.username}</Text>
                     <Flex gap={4} alignItems={'center'} justifyContent={'center'}>
-                        {visitingOwnProfileAndAuth && <Button bg={'white'} color={'black'} _hover={{ bg: 'whiteAlpha.800' }} size={{ base: 'xs', md: 'sm' }}>Edit Profile</Button>}
+                        {visitingOwnProfileAndAuth && <Button bg={'white'} color={'black'} _hover={{ bg: 'whiteAlpha.800' }} size={{ base: 'xs', md: 'sm' }} onClick={onOpen}>Edit Profile</Button>}
                         {visitingAnotherProfileAndAuth && <Button bg={'blue.500'} color={'white'} _hover={{ bg: 'blue.600' }} size={{ base: 'xs', md: 'sm' }}>Follow</Button>}
 
                     </Flex>
@@ -45,6 +46,7 @@ const ProfileHeader = () => {
                 </Flex>
                 <Text fontSize={'sm'}>{userProfile.bio}</Text>
             </VStack>
+            {isOpen && <EditProfile isOpen={isOpen} onClose={onClose} />}
         </Flex>
     )
 }
